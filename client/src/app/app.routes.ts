45864dd6dbe74458256523c81148d85fa3d374cb
zackmorgs <zackmorgenthaler@gmail.com>
2025-07-13 16:54:18 -0500

    fixed home rendering

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { 
     path: 'home', 
     loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
