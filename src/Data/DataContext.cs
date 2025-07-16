fe44c1a8382e7ec324d0a5b6828c6ebae38c8164
zackmorgs <zackmorgenthaler@gmail.com>
2025-07-16 06:04:08 -0500

    fixed database names, styled artist homepage album display

--- src/Data/DataContext.cs
+++ src/Data/DataContext.cs
@@ -15,9 +15,9 @@ namespace Data
         }
 
         public IMongoCollection<User> Users => _database.GetCollection<User>("users");
-        public IMongoCollection<Artist> Artists => _database.GetCollection<Artist>("Artists");
-        public IMongoCollection<Track> Tracks => _database.GetCollection<Track>("Tracks");
-        public IMongoCollection<Release> Releases => _database.GetCollection<Release>("Releases");
+        public IMongoCollection<Artist> Artists => _database.GetCollection<Artist>("artists");
+        public IMongoCollection<Track> Tracks => _database.GetCollection<Track>("tracks");
+        public IMongoCollection<Release> Releases => _database.GetCollection<Release>("releases");
         public IMongoCollection<MusicTag> MusicTags => _database.GetCollection<MusicTag>("tags");
     }
 }
