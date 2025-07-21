423011ce264449a8fa9077da6a2b9aabf6be630e
zackmorgs <zackmorgenthaler@gmail.com>
2025-07-21 05:00:16 -0500

    added Playlist model

---
+++ src/Models/Playlist.cs
@@ -0,0 +1,40 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace Models
+{
+    public class Playlist
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("title")]
+        [BsonRequired]
+        public string Title { get; set; } = string.Empty;
+
+        [BsonElement("userId")]
+        [BsonRequired]
+        public string UserId { get; set; } = string.Empty;
+
+        [BsonElement("createdAt")]
+        [BsonRequired]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [BsonElement("updatedAt")]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        [BsonElement("trackIds")]
+        public List<string> TrackIds { get; set; } = new List<string>();
+
+        [BsonElement("coverImageUrl")]
+        public string CoverImageUrl { get; set; } = string.Empty;
+
+        [BsonElement("description")]
+        public string Description { get; set; } = string.Empty;
+
+        [BsonElement("isPublic")]
+        public bool IsPublic { get; set; } = false;
+    }
+}
